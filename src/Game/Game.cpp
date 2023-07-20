a8953e9727938898eb910581cc70588f8b087ea4
Voyager_lqg7 <stepanenko.vadik@bk.ru>
2023-07-20 14:00:25 +0200

    Update: Level, Tank, Game classes. New item IGameStates.

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -1,60 +1,122 @@
-#include<glm/vec2.hpp>
-#include<glm/mat4x4.hpp>
-#include<glm/gtc/matrix_transform.hpp>
-#include"Game.h"
+#include "Game.h"
 
-#include"GameObjects/Tank.h"
-#include"../Renderer/ShaderProgram.h"
-#include"../Resources/ResourceManager.h"
-#include"../Renderer/Texture2D.h"
-#include"../Renderer/Sprite.h"
-#include"../Renderer/AnimatedSprite.h"
-#include<iostream>
+#include "../Resources/ResourceManager.h"
+#include "../Renderer/ShaderProgram.h"
+#include "../Renderer/Texture2D.h"
+#include "../Renderer/Sprite.h"
 
+#include "GameObjects/Tank.h"
+#include "GameObjects/Bullet.h"
 
-using namespace std;
+#include "GameStates/Level.h"
+#include "GameStates/StartScreen.h"
+#include "../Physics/PhysicsEngine.h"
+#include "../Renderer/Renderer.h"
 
-Game::Game(const glm::ivec2& windowSize)
-	: m_eCurrentGameState(EGameState::Active)
-    , m_windowSize(windowSize)
+#include <GLFW/glfw3.h>
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <iostream>
+
+Game::Game(const glm::uvec2& windowSize)
+    : m_windowSize(windowSize)
+    , m_eCurrentGameState(EGameState::StartScreen)
+    , m_currentLevelIndex(0)
 {
-	m_keys.fill(false);
+    m_keys.fill(false);
 }
-Game::~Game() {
+
+void Game::setWindowSize(const glm::uvec2& windowSize)
+{
+    m_windowSize = windowSize;
+    updateViewport();
+}
+
+Game::~Game()
+{
 
 }
 
-void Game::render() {
+void Game::render()
+{
+    m_pCurrentGameState->render();
 }
-void Game::update(const uint64_t delta) {
+
+void Game::updateViewport()
+{
+    const float level_aspect_ratio = static_cast<float>(getCurrentWidth()) / getCurrentHeight();
+    unsigned int viewPortWidth = m_windowSize.x;
+    unsigned int viewPortHeight = m_windowSize.y;
+    unsigned int viewPortLeftOffset = 0;
+    unsigned int viewPortBottomOffset = 0;
+
+    if (static_cast<float>(m_windowSize.x) / m_windowSize.y > level_aspect_ratio)
+    {
+        viewPortWidth = static_cast<unsigned int>(m_windowSize.y * level_aspect_ratio);
+        viewPortLeftOffset = (m_windowSize.x - viewPortWidth) / 2;
+    }
+    else
+    {
+        viewPortHeight = static_cast<unsigned int>(m_windowSize.x / level_aspect_ratio);
+        viewPortBottomOffset = (m_windowSize.y - viewPortHeight) / 2;
+    }
+
+    RenderEngine::Renderer::setViewport(viewPortWidth, viewPortHeight, viewPortLeftOffset, viewPortBottomOffset);
+
+    glm::mat4 projectionMatrix = glm::ortho(0.f, static_cast<float>(getCurrentWidth()), 0.f, static_cast<float>(getCurrentHeight()), -100.f, 100.f);
+    m_pSpriteShaderProgram->setMatrix4("projectionMat", projectionMatrix);
 }
-void Game::setkey(const int key, const int action) {
-	m_keys[key] = action;
+
+void Game::startNewLevel(const size_t level, const EGameMode eGameMode)
+{
+    m_currentLevelIndex = level;
+    auto pLevel = std::make_shared<Level>(ResourceManager::getLevels()[m_currentLevelIndex], eGameMode);
+    m_pCurrentGameState = pLevel;
+    Physics::PhysicsEngine::setCurrentLevel(pLevel);
+    updateViewport();
 }
-bool Game::init() {
+
+void Game::nextLevel(const EGameMode eGameMode)
+{
+    startNewLevel(++m_currentLevelIndex, eGameMode);
+}
+
+void Game::update(const double delta)
+{
+    m_pCurrentGameState->processInput(m_keys);
+    m_pCurrentGameState->update(delta);
+}
+
+void Game::setKey(const int key, const int action)
+{
+    m_keys[key] = action;
+}
+
+bool Game::init()
+{
     ResourceManager::loadJSONResources("res/resources.json");
-    auto pSpriteShaderProgram = ResourceManager::getShaderProgram("spriteShader");
-    if (!pSpriteShaderProgram) {
-        cerr << "Cant find shader program "<< "spriteShader" << endl;
+
+    m_pSpriteShaderProgram = ResourceManager::getShaderProgram("spriteShader");
+    if (!m_pSpriteShaderProgram)
+    {
+        std::cerr << "Can't find shader program: " << "spriteShader" << std::endl;
         return false;
     }
+    m_pSpriteShaderProgram->use();
+    m_pSpriteShaderProgram->setInt("tex", 0);
 
-    auto pTextureAtlas = ResourceManager::getTexture("mapTextureAtlas");
-    if (!pTextureAtlas) {
-        cerr << "Cant find texture atlas " << "mapTextureAtlas" << endl;
-        return false;
-    }
+    m_pCurrentGameState = std::make_shared<StartScreen>(ResourceManager::getStartScreen(), this);
+    setWindowSize(m_windowSize);
 
-    glm::mat4 modelMatrix_1 = glm::mat4(1.f);
-    modelMatrix_1 = glm::translate(modelMatrix_1, glm::vec3(100.f, 200.f, 0.f));
-
-    glm::mat4 modelMatrix_2 = glm::mat4(1.f);
-    modelMatrix_2 = glm::translate(modelMatrix_2, glm::vec3(590.f, 200.f, 0.f));
-
-    glm::mat4 projectionMatrix = glm::ortho(0.f, static_cast<float>(m_windowSize.x), 0.f, static_cast<float>(m_windowSize.y), -100.f, 100.f);
-
-    pSpriteShaderProgram->use();
-    pSpriteShaderProgram->setInt("tex", 0);
-    pSpriteShaderProgram->setMatrix4("projectionMat", projectionMatrix);
     return true;
+}
+
+unsigned int Game::getCurrentWidth() const
+{
+    return m_pCurrentGameState->getStateWidth();
+}
+
+unsigned int Game::getCurrentHeight() const
+{
+    return m_pCurrentGameState->getStateHeight();
 }
--- src/Game/Game.h
+++ src/Game/Game.h
@@ -1,23 +1,45 @@
 #pragma once
-#include<iostream>
-#include<array>
-#include<glm/vec2.hpp>
-using namespace std;
+
+#include <glm/vec2.hpp>
+#include <array>
+#include <memory>
+class IGameState;
+namespace RenderEngine {
+    class ShaderProgram;
+}
+
 class Game {
 public:
-	Game(const glm::ivec2& windowSize);
-	~Game();
+    enum class EGameMode {
+        OnePlayer,
+        TwoPlayers
+    };
+    Game(const glm::uvec2& windowSize);
+    ~Game();
+    void render();
+    void update(const double delta);
+    void setKey(const int key, const int action);
+    bool init();
+    unsigned int getCurrentWidth() const;
+    unsigned int getCurrentHeight() const;
+    void startNewLevel(const size_t level, const EGameMode eGameMode);
+    void nextLevel(const EGameMode eGameMode);
+    void updateViewport();
+    void setWindowSize(const glm::uvec2& windowSize);
 
-	void render();
-	void update(const uint64_t detla);
-	void setkey(const int key, const int action);
-	bool init();
 private:
-	array<bool, 349> m_keys;
-	enum class EGameState {
-		Active,
-		Pause
-	};
-	glm::ivec2 m_windowSize;
-	EGameState m_eCurrentGameState;
+    enum class EGameState {
+        StartScreen,
+        LevelStart,
+        Level,
+        Pause,
+        Scores,
+        GameOver
+    };
+    std::array<bool, 349> m_keys;
+    glm::ivec2 m_windowSize;
+    EGameState m_eCurrentGameState;
+    std::shared_ptr<IGameState> m_pCurrentGameState;
+    std::shared_ptr<RenderEngine::ShaderProgram> m_pSpriteShaderProgram;
+    size_t m_currentLevelIndex;
 };
